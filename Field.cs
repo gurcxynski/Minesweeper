d4c5c8ff5ad53e2ab6470622c7621c91d6aeb10d
Wojciech Górczyński <7305532u@prus.siedlce.pl>
2022-03-28 19:26:21 +0200

    Add project files.

---
+++ Field.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using MonoGame.EasyInput;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Minesweeper
+{
+    public class Field
+    {
+        enum State { hidden, clicked, flagged }
+        State state = State.hidden;
+        public bool hasMine = false;
+        public int value = 0;
+        Vector2 Position = new Vector2();
+        public List<Field> linked = new List<Field>();
+        Texture2D texture;
+        public Field(int x, int y)
+        {
+            Position = new Vector2(x, y);
+            if (Globals.rand.Next(100) > 90) hasMine = true;
+            texture = Globals.field;
+        }
+        public void Draw()
+        {
+            Globals.spriteBatch.Begin();
+            Globals.spriteBatch.Draw(texture, 50 * Position, Color.White);
+            if(state == State.clicked) Globals.spriteBatch.DrawString(Globals.font, value.ToString(), Position * 50 + new Vector2(20, 10), Color.Black);
+            Globals.spriteBatch.End();
+        }
+        public void Link(Vector2 arg)
+        {
+            if (Globals.fields.ContainsKey(arg))
+            {
+                linked.Add(Globals.fields[arg]);
+                Globals.fields[arg].linked.Add(this);
+            }
+        }
+        public void Calc()
+        {
+            foreach (var item in linked)
+            {
+                if (item.hasMine) value++;
+            }
+        }
+        public void Reveal()
+        {
+            if (hasMine)
+            {
+                texture = Globals.mine;
+                return;
+            }
+            state = State.clicked;
+            texture = Globals.field2;
+            if (value == 0)
+            {
+                foreach (var item in linked)
+                {
+                    item.state = State.clicked;
+                    item.texture = Globals.field2;
+                }
+            }
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.EasyInput;
+using System;
+using System.Collections.Generic;
+
+namespace Minesweeper
+{
+    public static class Globals
+    {
+        public static int size = 10;
+        public static SpriteBatch spriteBatch;
+        public static Texture2D field;
+        public static Texture2D field2;
+        public static Texture2D mine;
+        public static SpriteFont font;
+        public static Dictionary<Vector2, Field> fields = new Dictionary<Vector2, Field>();
+        public static Random rand = new Random();
+        public static EasyMouse mouse = new EasyMouse();
+    }
+    public class Game1 : Game
+    {
+        readonly GraphicsDeviceManager graphics;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+
+            Globals.spriteBatch = new SpriteBatch(GraphicsDevice);
+            Globals.field = Content.Load<Texture2D>("field");
+            Globals.field2 = Content.Load<Texture2D>("field2");
+            Globals.font = Content.Load<SpriteFont>("fonts/MarkerFelt-16");
+            Globals.mine = Content.Load<Texture2D>("mine");
+
+
+            IsMouseVisible = true;
+            graphics.PreferredBackBufferHeight = graphics.PreferredBackBufferWidth = 50 * Globals.size;
+            graphics.ApplyChanges();
+
+            for (int i = 0; i < Globals.size; i++)
+            {
+                for (int j = 0; j < Globals.size; j++)
+                {
+                    Globals.fields.Add(new Vector2(i, j), new Field(i, j));
+                }
+            }
+            foreach (var item in Globals.fields)
+            {
+                item.Value.Link(new Vector2(item.Key.X + 1, item.Key.Y));
+                item.Value.Link(new Vector2(item.Key.X,     item.Key.Y + 1));
+                item.Value.Link(new Vector2(item.Key.X + 1, item.Key.Y + 1));
+                item.Value.Link(new Vector2(item.Key.X - 1, item.Key.Y + 1));
+            }
+            foreach (var item in Globals.fields.Values)
+            {
+                item.Calc();
+            }
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            Globals.mouse.Update();
+            if (Globals.mouse.ReleasedThisFrame(MouseButtons.Left))
+            {
+                Vector2 pos = new Vector2(Globals.mouse.Position.X / 50, Globals.mouse.Position.Y / 50);
+                if (Globals.fields.ContainsKey(pos)) Globals.fields[pos].Reveal();
+            }
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            foreach (var item in Globals.fields.Values)
+            {
+                item.Draw();
+            }
+            base.Draw(gameTime);
+        }
+
+    }
+}
